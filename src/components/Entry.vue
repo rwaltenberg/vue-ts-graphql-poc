12df5c540be341a56554f9885bde16b3572ae17d
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2019-02-27 18:27:43 -0300

    Adiciona o componente de pokedex e entry com dummy data

---
+++ src/components/Entry.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Vue from 'vue';
+import { kebabCase } from 'lodash';
+
+export default Vue.extend({
+    name: 'Entry',
+    props: {
+        entryData: Object,
+    },
+    methods: {
+        getImageUrl(num: number): string {
+            return `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`;
+        },
+        slugify: kebabCase,
+    },
+});
+</script>
+
+<template lang="pug">
+    .entry
+        .image
+            img(:src="getImageUrl(entryData.number)")
+        .number
+            | \#{{ entryData.number }}
+        .name
+            | {{ entryData.name }}
+        .types
+            .type(v-for="t in entryData.types" :class="slugify(t)")
+                | {{ t }}
+</template>
+
---
+++ src/components/Pokedex.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import Vue from 'vue';
+import Entry from './Entry.vue';
+
+export default Vue.extend({
+    name: 'Pokedex',
+    components: {
+        Entry,
+    },
+    data() {
+        return {
+            pokemons: [{
+                number: '025',
+                name: 'pikachu',
+                types: ['eletric'],
+            }],
+        };
+    },
+});
+</script>
+
+<template lang="pug">
+    .pokedex
+        entry(v-for="poke of pokemons" :key="poke.number" :entry-data="poke")
+</template>
+
