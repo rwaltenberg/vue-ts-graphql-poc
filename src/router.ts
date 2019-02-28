c91a09c794fc3fee20288648ca73292c17f825b1
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2019-02-28 17:33:31 -0300

    Remove a página About

--- src/router.ts
+++ src/router.ts
@@ -8,18 +8,19 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    // },
     {
       path: '/:id?',
       name: 'entries',
       component: Home,
+    },
+    {
+      path: '*',
+      redirect: { name: 'entries' },
     },
   ],
 });
